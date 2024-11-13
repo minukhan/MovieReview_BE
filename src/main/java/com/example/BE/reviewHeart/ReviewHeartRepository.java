17a47b973c4a077d250ecd4ac50f8fa24c957b10
minwook <minukhan>
2024-11-13 11:13:52 +0900

    feat: ReviewLike API

--- src/main/java/com/example/BE/reviewHeart/ReviewHeartRepository.java
+++ src/main/java/com/example/BE/reviewHeart/ReviewHeartRepository.java
@@ -1,4 +1,17 @@
 package com.example.BE.reviewHeart;
 
-public interface ReviewHeartRepository {
+
+import com.example.BE.review.ReviewEntity;
+import com.example.BE.user.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ReviewHeartRepository extends JpaRepository<ReviewHeartEntity, Integer> {
+
+    // 특정 사용자와 리뷰 조합의 좋아요가 이미 존재하는지 확인하는 메서드
+    boolean existsByUserAndReview(UserEntity user, ReviewEntity review);
+
+    // 특정 사용자와 리뷰 조합의 좋아요를 조회하는 메서드
+    Optional<ReviewHeartEntity> findByUserAndReview(UserEntity user, ReviewEntity review);
 }
