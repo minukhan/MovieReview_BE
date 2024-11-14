4af3bf3a520e9b13725ec600ea93b89ccf6fd4c2
inwoo0808 <inwoo0808@gmail.com>
2024-11-14 11:06:27 +0900

    사용자 기반 추천 영화 list 반환 api 작성 완료

---
+++ src/main/java/com/example/BE/recommend/service/RecommendService.java
@@ -0,0 +1,7 @@
+package com.example.BE.recommend.service;
+
+import com.example.BE.recommend.RecommendEntity;
+
+public interface RecommendService {
+    public void submit_recommend(RecommendEntity recommend);
+}
