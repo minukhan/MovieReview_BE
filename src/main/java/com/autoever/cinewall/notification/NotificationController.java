9a573da0f9f3a4e985e6402a26cd3c55b96885d8
inwoo0808 <inwoo0808@gmail.com>
2024-11-16 00:32:49 +0900

    sse연결2

--- src/main/java/com/autoever/cinewall/notification/NotificationController.java
+++ src/main/java/com/autoever/cinewall/notification/NotificationController.java
@@ -3,12 +3,15 @@ package com.autoever.cinewall.notification;
 import com.autoever.cinewall.user.UserEntity;
 import com.autoever.cinewall.user.service.UserService;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 @RestController
@@ -21,6 +24,9 @@ public class NotificationController {
     //@CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
     @GetMapping(value = "/subscribe", produces = "text/event-stream;charset=UTF-8")
     public SseEmitter subscribe(HttpServletRequest request) {
+        HttpServletResponse response = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getResponse();
+        response.setHeader("Content-Type", "text/event-stream;charset=UTF-8");
+
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         String userName = auth.getName();
         UserEntity user = userService.findById(userName);
