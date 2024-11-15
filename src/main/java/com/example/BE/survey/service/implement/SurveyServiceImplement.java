72e4bb4982f1d2c97b82e1ec29f8700f5a499169
inwoo0808 <inwoo0808@gmail.com>
2024-11-15 15:03:46 +0900

    recommend 수정 및 불필요 table 삭제

--- src/main/java/com/example/BE/survey/service/implement/SurveyServiceImplement.java
+++ src/main/java/com/example/BE/survey/service/implement/SurveyServiceImplement.java
@@ -1,11 +1,16 @@
 package com.example.BE.survey.service.implement;
 
+import com.example.BE.genre.GenreEntity;
+import com.example.BE.recommend.RecommendEntity;
 import com.example.BE.recommend.RecommendRepository;
 import com.example.BE.survey.SurveyEntity;
 import com.example.BE.survey.SurveyRepository;
 import com.example.BE.survey.service.SurveyService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -15,6 +20,12 @@ public class SurveyServiceImplement implements SurveyService {
 
     public void submit_survey(SurveyEntity surveyEntity){
         surveyRepository.save(surveyEntity);
+        List<GenreEntity> genres = surveyRepository.findGenresByUserId(surveyEntity.getUser().getUserId());
+        RecommendEntity recommend = RecommendEntity.builder()
+                .genres(genres)
+                .user(surveyEntity.getUser())
+                .build();
+        recommendRepository.save(recommend);
         return;
     }
 
