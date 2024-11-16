0818ce763b107a125c5cd7ec2e2d0afd1044669a
inwoo0808 <inwoo0808@gmail.com>
2024-11-16 13:48:11 +0900

    sse 연결 7

--- src/main/java/com/autoever/cinewall/notification/NotificationController.java
+++ src/main/java/com/autoever/cinewall/notification/NotificationController.java
@@ -7,11 +7,13 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
@@ -23,9 +25,11 @@ public class NotificationController {
     private final NotificationService notificationService;
     private final UserService userService;
 
+    //@GetMapping(value = "/subscribe", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+
     @CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
-    @GetMapping(value = "/subscribe", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public SseEmitter subscribe(HttpServletRequest request) {
+    @GetMapping(value = "/subscribe")
+    public ResponseEntity<SseEmitter> subscribe(HttpServletRequest request) {
         //HttpServletResponse response = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getResponse();
         //response.setHeader("Content-Type", "text/event-stream;charset=UTF-8");
 
@@ -33,7 +37,14 @@ public class NotificationController {
         String userName = auth.getName();
         UserEntity user = userService.findById(userName);
 
-        return notificationService.subscribe(user.getUserId());
+        SseEmitter emitter = notificationService.subscribe(user.getUserId());
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set(HttpHeaders.CONTENT_TYPE, "text/event-stream;charset=UTF-8");
+        headers.set(HttpHeaders.CACHE_CONTROL, "no-cache");
+        headers.set(HttpHeaders.CONNECTION, "keep-alive");
+
+        return new ResponseEntity<>(emitter, headers, HttpStatus.OK);
     }
 
 }
